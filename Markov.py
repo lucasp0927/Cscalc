9eef852d5b83d6ab83b8f709a713c439ca4b741a
Lucas Peng <lucasp0927@gmail.com>
2012-02-25 00:21:10 +0800

    clean code

--- Markov.py
+++ Markov.py
@@ -11,7 +11,6 @@ HBAR =  1.05457148e-34
 class Markov(object):
     """
     """
-    
     def __init__(self, ):
         """
         """
@@ -22,23 +21,21 @@ class Markov(object):
         self.omega = self.parameter['omega']
         self.group = self.parameter['level_group']
         self.dipole = self.parameter['dipole'][0]
-        #print self.dipole
         self.n = self.parameter['n']# number of levels
         self.N = self.n**2 # the number of independent terms in  density matrix
         self.decoherence = self.parameter['decoherence_matrix']
-        
+
         self.T = np.zeros((self.N,self.N),complex) # time independent part d rho/ dt = T rho
-        self.D = np.zeros((self.N,self.N),complex) # time independent part d rho/ dt = T rho        
+        self.D = np.zeros((self.N,self.N),complex) # time independent part d rho/ dt = T rho
         self.final = np.zeros((self.N,self.N),complex) # final markov matrix
 
-        
         self.EField = ElectricField()
         self.smpnum = self.EField.sample
         self.cutoff = self.EField.cutoff
         self.tsample = np.linspace(0,self.EField.cutoff,self.smpnum)
         #self.Dfunction = [np.zeros((self.N,self.N),complex) for i in range(self.EField.sample)] # time dependent part
         self.Dfunction = np.empty((self.N,self.N,self.smpnum),complex)
-        self.DfunctionTemp = np.empty((self.N,self.N,self.smpnum),complex)        
+        self.DfunctionTemp = np.empty((self.N,self.N,self.smpnum),complex)
         self.order = 0
         dictf.close()
 
@@ -60,26 +57,26 @@ class Markov(object):
                 return self.omega[i]-self.omega[j]
         else:
             return self.omega[i]-self.omega[j]
-        
+
     def prepareT(self):
         for i in range(self.n):
             for j in range(i,self.n):
                 for k in self.decoherence[i][j]:
                     self.T[self.ij2idx(i,j)][self.ij2idx(k[0],k[1])]+=k[2]
-                    self.T[self.ij2idx(j,i)][self.ij2idx(k[1],k[0])]+=k[2]                    
+                    self.T[self.ij2idx(j,i)][self.ij2idx(k[1],k[0])]+=k[2]
         for i in range(self.n):
              for j in range(i+1,self.n):
                  self.T[self.ij2idx(i,j)][self.ij2idx(i,j)]+= -1.0j*self.rotate_omega(i,j)
-                 self.T[self.ij2idx(j,i)][self.ij2idx(j,i)]+= 1.0j*self.rotate_omega(i,j)             
-                 
+                 self.T[self.ij2idx(j,i)][self.ij2idx(j,i)]+= 1.0j*self.rotate_omega(i,j)
+
     def prepareD(self):
         for i in range(self.n):
             for j in range(self.n):
                 for k in range(self.n):
                     self.D[self.ij2idx(i,j)][self.ij2idx(k,j)] += -1.0j*(self.dipole[i][k] )/ HBAR
-                    self.D[self.ij2idx(i,j)][self.ij2idx(i,k)] -= -1.0j*(self.dipole[k][j] )/ HBAR                    
-                
-    
+                    self.D[self.ij2idx(i,j)][self.ij2idx(i,k)] -= -1.0j*(self.dipole[k][j] )/ HBAR
+
+
     def zeroOrder(self):
         for i in enumerate(self.tsample):
             print i[0]
@@ -90,15 +87,13 @@ class Markov(object):
             for j in range(i,int(self.N)):
                 print i,j
                 self.integrate(i,j)
-
-        self.order += 1            
+        self.order += 1
         self.Dfunction = self.DfunctionTemp
-        
+
     def calcSlope(self,I,J,i):
         ans = 0.0 + 0.0j
         for k in range(self.N):
             ans += (self.T[I][k]+self.EField.envelope(self.tsample[i])*self.D[I][k])*self.Dfunction[k,J,i]
-            #ans += self.EField.envelope(self.tsample[i])
         return ans
 
     def integrate(self,I,J):
@@ -106,10 +101,6 @@ class Markov(object):
             init = 1
         else:
             init = 0
-        # slope = np.zeros(self.smpnum,complex) 
-        # for i in range(self.smpnum):
-        #     for k in range(self.N):
-        #         slope[i] += (self.T[I][k]+self.EField.envelope(self.tsample[i])*self.D[I][k])*self.Dfunction[k,J,i]
         slopfunc = np.vectorize(self.calcSlope)
         slope = slopfunc(I,J,np.arange(self.smpnum))
         # interpolate slope
@@ -126,7 +117,6 @@ class Markov(object):
                 return ys[-1]+(t-xs[-1])*(ys[-1]-ys[-2])/(xs[-1]-xs[-2])
             else:
                 return interpolater(t)
-            #return 0
         result_r = integrate.odeint(slp,init,self.tsample,args=(slope_r_int,))
         result_i = integrate.odeint(slp,init,self.tsample,args=(slope_i_int,))
         self.DfunctionTemp[I,J,:] = np.transpose(result_r + 1.0j*result_i)
@@ -134,12 +124,11 @@ class Markov(object):
     def finalResult(self):
         time = 2*np.pi/self.EField.repetition_freq-self.EField.cutoff
         self.final = np.dot(self.Dfunction[:,:,-1],linalg.expm(self.T*time)) # check this
-    
+
 if __name__ == '__main__':
     markov = Markov()
     markov.prepareT()
-    markov.prepareD()    
+    markov.prepareD()
     markov.zeroOrder()
     markov.addOrder()
-
-
+    markov.finalResult()
