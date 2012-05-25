6d61a755915fafda937fdbf526dc866ee2d8ae6b
Lucas <lucasp0927@gmail.com>
2012-05-25 14:49:25 +0800

    remove scipy in Electricfield

--- ElectricField.py
+++ ElectricField.py
@@ -2,12 +2,11 @@
 import numpy as np
 #import matplotlib.pyplot as plt
 from pylab import plot, show, title, xlabel, ylabel, subplot
-from scipy import fft, integrate
+#from scipy import fft, integrate
 
 class ElectricField(object):
     """
     """
-    
     def __init__(self,):
         """
         """
@@ -32,48 +31,58 @@ class ElectricField(object):
         """
         return self.maxima*np.exp(-(t-self.sigma*5)**2/(2*self.sigma**2))
 
-    def plotSpectrum(self,y,Ts):
-        """
-        Plots a Single-Sided Amplitude Spectrum of y(t)
-        """
-        Fs = 1/Ts
-        n = len(y) # length of the signal
-        k = np.arange(n)
-        T = n/Fs
-        frq = k/T # two sides frequency range
-        frq = frq[range(n/2)] # one side frequency range
+    # def plotSpectrum(self,y,Ts):
+    #     """
+    #     Plots a Single-Sided Amplitude Spectrum of y(t)
+    #     """
+    #     Fs = 1/Ts
+    #     n = len(y) # length of the signal
+    #     k = np.arange(n)
+    #     T = n/Fs
+    #     frq = k/T # two sides frequency range
+    #     frq = frq[range(n/2)] # one side frequency range
     
-        Y = fft(y)/n # fft computing and normalization
-        Y = Y[range(n/2)]
+    #     Y = fft(y)/n # fft computing and normalization
+    #     Y = Y[range(n/2)]
         
-        plot(frq,abs(Y),'pr') # plotting the spectrum
-        xlabel('Freq (Hz)')
-        ylabel('|Y(freq)|')
+    #     plot(frq,abs(Y),'pr') # plotting the spectrum
+    #     xlabel('Freq (Hz)')
+    #     ylabel('|Y(freq)|')
 
     def calpower(self,):
-        return '{:e}'.format(3e8*8.85e-12/2*integrate.romberg(lambda x:(self.envelope(x)*np.sin(self.carrier_freq*x))**2,0,self.cutoff,divmax=20)/(2*np.pi/self.repetition_freq))+"w/m^2"
+        # power1 = 3e8*8.85e-12/2*integrate.romberg(lambda x:(self.envelope(x)*np.sin(self.carrier_freq*x))**2,0,self.cutoff,divmax=20)/(2*np.pi/self.repetition_freq)
+        # power2 = 3e8*8.85e-12/2*integrate.romberg(lambda x:(self.envelope(x)**2),0,self.cutoff,divmax=20)/(2*np.pi/self.repetition_freq)/2.0
+        t = np.linspace(0,self.cutoff,2000)
+        e = self.envelope(t)
+        e = (e**2)*(t[1]-t[0])
+        power3 = np.sum(e)*3e8*8.85e-12/2/(2*np.pi/self.repetition_freq)/2.0
+        # print '{:e}'.format(power1)+"w/m^2"
+        # print '{:e}'.format(power2)+"w/m^2"
+        return '{:e}'.format(power3)+"w/m^2"                  
+    #return '{:e}'.format(3e8*8.85e-12/2*integrate.romberg(lambda x:(self.envelope(x)*np.sin(self.carrier_freq*x))**2,0,self.cutoff,divmax=20)/(2*np.pi/self.repetition_freq))+"w/m^2"
     
-    def check(self,):
-        print "rabi frequency area: "
-        print 4e5*integrate.quad(self.envelope,0,self.cutoff)[0] #4e5 is dipole moment / hbar
-        print "average power:"
-        print self.calpower()
-        print str(3e8*8.85e-12/4*integrate.romberg(lambda x:(self.envelope(x))**2,0,self.cutoff)/(2*np.pi/self.repetition_freq))+"w/m^2"        
-        Ts = self.cutoff/500.0
-        x = np.arange(0, self.cutoff, Ts)
-        env_vec = np.vectorize(self.envelope)
-        y = env_vec(x)
-        print "please check if rotating wave approximation is valid."
-        # plot envelope and its spectrum
-        subplot(2,1,1)
-        plot(x,y)
-        xlabel('Time')
-        ylabel('Amplitude')
-        subplot(2,1,2)
-        self.plotSpectrum(y,Ts)
-        show()
+    # def check(self,):
+    #     print "rabi frequency area: "
+    #     print 4e5*integrate.quad(self.envelope,0,self.cutoff)[0] #4e5 is dipole moment / hbar
+    #     print "average power:"
+    #     print self.calpower()
+    #     print str(3e8*8.85e-12/4*integrate.romberg(lambda x:(self.envelope(x))**2,0,self.cutoff)/(2*np.pi/self.repetition_freq))+"w/m^2"        
+    #     Ts = self.cutoff/500.0
+    #     x = np.arange(0, self.cutoff, Ts)
+    #     env_vec = np.vectorize(self.envelope)
+    #     y = env_vec(x)
+    #     print "please check if rotating wave approximation is valid."
+    #     # plot envelope and its spectrum
+    #     subplot(2,1,1)
+    #     plot(x,y)
+    #     xlabel('Time')
+    #     ylabel('Amplitude')
+    #     subplot(2,1,2)
+    #     #self.plotSpectrum(y,Ts)
+    #     show()
 
 if __name__ == '__main__':
     ef = ElectricField()
-    ef.check()
+    ef.calpower()
+    #    ef.check()
 
